d1302650896c006313370afaaff9b1c6653c94e7
Özlem ÖZKAYA <58657181+ozlemozk@users.noreply.github.com>
2020-05-31 16:03:31 +0300

    Update mock_arduino.cpp

--- mock_arduino.cpp
+++ mock_arduino.cpp
@@ -1,25 +1,7 @@
-/*
-  DSM2_tx implements the serial communication protocol used for operating
-  the RF modules that can be found in many DSM2-compatible transmitters.
 
-  Copyrigt (C) 2012  Erik Elmore <erik@ironsavior.net>
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.  
-*/
 
 #include <sys/timeb.h>
-#include "mock_arduino.h"
+#include "ArduinoTest2_arduino.h"
 
 timeb t_start;
 unsigned long millis() {
@@ -33,6 +15,6 @@ void delay(unsigned long ms) {
   while(millis() - start < ms){}
 }
 
-void initialize_mock_arduino() {
+void initialize_ArduinoTest2_arduino() {
   ftime(&t_start);
 }
