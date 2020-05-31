d32863ea07c60fdeca44b66bf03c529750e0df8e
Esra Yüce <61189350+Esra320@users.noreply.github.com>
2020-05-31 16:49:20 +0300

    Update RunTest.cpp

--- Arduino_Test/RunTest.cpp
+++ Arduino_Test/RunTest.cpp
@@ -1,24 +1,5 @@
-/*
-  DSM2_tx implements the serial communication protocol used for operating
-  the RF modules that can be found in many DSM2-compatible transmitters.
 
-  Copyrigt (C) 2012  Erik Elmore <erik@ironsavior.net>
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.  
-*/
-
-#include "mock_arduino.h"
+#include "ArduinoTest2_arduino.h"
 #include "dsm2_tx.h"
 
 using namespace std;
@@ -31,7 +12,7 @@ void millis_test() {
     sleep(1);
   }
   unsigned long end = millis();
-  cout << "End of test - duration: " << end - start << "ms" << endl;
+  cout << " Sonuç - Süre: " << end - start << "ms" << endl;
 }
 
 void delay_test() {
@@ -42,16 +23,16 @@ void delay_test() {
     delay(250);
   }
   unsigned long end = millis();
-  cout << "End of test - duration: " << end - start << "ms" << endl;
+  cout << "Sonuç - Süre: " << end - start << "ms" << endl;
 }
 
-void run_tests();
+void Run_Test();
 int main(int argc, char **argv){
   initialize_mock_arduino();
-  run_tests();
+  Run_Test();
 }
 
-void run_tests() {
+void Run_Test() {
   DSM2_tx tx(6);
   tx.bind();
   for( int i = 0; i < 6; i++) {
